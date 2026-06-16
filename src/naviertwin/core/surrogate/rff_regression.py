44a8588fe42b72c8ccd3458b53978baf4d9f2648
younglin90 <younglin90@gmail.com>
2026-06-17 07:00:59 +0900

    experiment: move rff regression solve to c++ native

--- src/naviertwin/core/surrogate/rff_regression.py
+++ src/naviertwin/core/surrogate/rff_regression.py
@@ -17,7 +17,11 @@ from __future__ import annotations
 import numpy as np
 from numpy.typing import NDArray
 
+from naviertwin._native import _kernels
 from naviertwin.core.neural.positional_enc import gaussian_rff
+
+if _kernels is None:  # pragma: no cover
+    raise ImportError("NavierTwin native kernels are required")
 
 
 class RFFRegression:
@@ -39,7 +43,7 @@ class RFFRegression:
     def fit(self, X: NDArray[np.float64], y: NDArray[np.float64]) -> "RFFRegression":
         Phi = self._features(X)
         A = Phi.T @ Phi + self.ridge * np.eye(Phi.shape[1])
-        self.W_out = np.linalg.solve(A, Phi.T @ np.asarray(y).ravel())
+        self.W_out = np.asarray(_kernels.solve_square(A, Phi.T @ np.asarray(y).ravel()), dtype=np.float64)
         return self
 
     def predict(self, X: NDArray[np.float64]) -> NDArray[np.float64]:
